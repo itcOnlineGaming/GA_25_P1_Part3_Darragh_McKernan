2ef64648171a5ec4069a7c4937f12d27492e10e3
DarraghMcKernan <dmckernan77@gmail.com>
2025-04-02 22:24:13 +0100

    #3.4 MAU calculation and visualisation

---
+++ MonthlyActiveUsers.py
@@ -0,0 +1,8 @@
+import pandas as pd
+def GetMonthlyActiveUsers():
+
+    monthlyLogins = pd.read_csv("C:\\Users\\Darragh\\Desktop\\Programming\\Year 4\\Game Analytics\\GA_25_P1_Part3_Darragh_McKernan\\data\\player_logged_in.csv",parse_dates=["Time"],dayfirst=True)
+    monthlyLogins["Month"] = monthlyLogins["Time"].dt.to_period("M").astype(str)
+    monthlyLogins = monthlyLogins.groupby("Month")["pid"].nunique().reset_index(name="MAU")
+
+    return monthlyLogins.sort_values("Month")
